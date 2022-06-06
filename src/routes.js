5efb53c0526e15e920fe35ef8e80d2c2f7d7675b
Vitor Melo <vitormelo9913@gmail.com>
2022-06-06 20:13:50 -0300

    Controller Users

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from "express";
-import User from "./app/models/User";
-import Database from './database/index';
+import UserController from "./app/Controllers/UserController";
+import Database from "./database/index";
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-    // return res.json({
-    //     message: "Okay",
-    // });
-    const user = await User.create({
-        name: "Vitor andrade",
-        email: "vitor@email.com",
-        password_hash: "12345679",
-    });
-
-    return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
